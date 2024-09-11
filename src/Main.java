b6a3ff11fee3046efc81fd0c04ce9f9ba899730f
fkbinho <sir_kleber@hotmail.com>
2024-09-11 12:31:42 -0300

    add Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.fkbinho.desafio.dominio.Curso;
+import br.com.fkbinho.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Aprendendo Linguagem Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScrip");
+        curso2.setDescricao("Aprendendo JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria primeiro emprego Java");
+        mentoria.setDescricao("Passo a passo para conseguir o 1º emprego");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
